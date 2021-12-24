ece53f2ba2c29e7ceaafe15b61bd16a7d30eba5c
EliasZeineldin <96598363+EliasZeineldin@users.noreply.github.com>
2021-12-24 14:56:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function color(){
+    document.getElementById("para1").style.color="Aqua"
+}
+function color2(){
+    document.getElementById("para2").style.color="Purple"
+}
+function color3(){
+    document.getElementById("para3").style.color="Gold"
+}
+function imgchng(){
+    document.getElementById("img1").src="https://images.unsplash.com/photo-1521731978332-9e9e714bdd20?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzODk5NDY4Mw&ixlib=rb-1.2.1&q=85";
+
+}
+function imgchng2(){
+    document.getElementById("img2").src="https://images.unsplash.com/photo-1628314200733-5f7785cdc925?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzODk5NDI0OQ&ixlib=rb-1.2.1&q=85";
+}
